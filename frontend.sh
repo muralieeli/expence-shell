c66b165bf9b5c7958456b0670033c68747825765
muralieeli <empkrishna@gmail.com>
2023-11-02 23:17:54 +0530

    chenges

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,13 @@
-dnf install nginx -y
+dnf install nginx -y &>> /tmp/out
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>> /tmp/out
 rm -rf /usr/share/nginx/html/*
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> /tmp/out
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>> /tmp/out
+unzip /tmp/frontend.zip &>> /tmp/out
 
-systemctl restart nginx
+systemctl restart nginx &>> /tmp/out
 
 
